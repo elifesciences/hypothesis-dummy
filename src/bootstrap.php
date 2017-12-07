a7cf882d5fff28650c65251189d732f2dae85223
Giorgio Sironi <g.sironi@elifesciences.org>
2017-12-07 15:22:20 +0000

    Implementing /

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -22,6 +22,101 @@ $app->post('/users', function (Request $request) {
     ));
 });
 
+$app->get('/', function (Request $request) {
+    $base = $request->getScheme().'://'.$request->getHost();
+$body = array (
+  'message' => 'Annotator Store API',
+  'links' => 
+  array (
+    'profile' => 
+    array (
+      'read' => 
+      array (
+        'url' => $base.'/profile',
+        'method' => 'GET',
+        'desc' => 'Fetch the user\'s profile',
+      ),
+      'update' => 
+      array (
+        'url' => 'https://hypothes.is/api/profile',
+        'method' => 'PATCH',
+        'desc' => 'Update a user\'s preferences',
+      ),
+    ),
+    'search' => 
+    array (
+      'url' => 'https://hypothes.is/api/search',
+      'method' => 'GET',
+      'desc' => 'Search for annotations',
+    ),
+    'group' => 
+    array (
+      'member' => 
+      array (
+        'delete' => 
+        array (
+          'url' => 'https://hypothes.is/api/groups/:pubid/members/:user',
+          'method' => 'DELETE',
+          'desc' => 'Remove the current user from a group.',
+        ),
+      ),
+    ),
+    'annotation' => 
+    array (
+      'hide' => 
+      array (
+        'url' => 'https://hypothes.is/api/annotations/:id/hide',
+        'method' => 'PUT',
+        'desc' => 'Hide an annotation as a group moderator.',
+      ),
+      'unhide' => 
+      array (
+        'url' => 'https://hypothes.is/api/annotations/:id/hide',
+        'method' => 'DELETE',
+        'desc' => 'Unhide an annotation as a group moderator.',
+      ),
+      'read' => 
+      array (
+        'url' => 'https://hypothes.is/api/annotations/:id',
+        'method' => 'GET',
+        'desc' => 'Fetch an annotation',
+      ),
+      'create' => 
+      array (
+        'url' => 'https://hypothes.is/api/annotations',
+        'method' => 'POST',
+        'desc' => 'Create an annotation',
+      ),
+      'update' => 
+      array (
+        'url' => 'https://hypothes.is/api/annotations/:id',
+        'method' => 'PATCH',
+        'desc' => 'Update an annotation',
+      ),
+      'flag' => 
+      array (
+        'url' => 'https://hypothes.is/api/annotations/:id/flag',
+        'method' => 'PUT',
+        'desc' => 'Flag an annotation for review.',
+      ),
+      'delete' => 
+      array (
+        'url' => 'https://hypothes.is/api/annotations/:id',
+        'method' => 'DELETE',
+        'desc' => 'Delete an annotation',
+      ),
+    ),
+    'links' => 
+    array (
+      'url' => 'https://hypothes.is/api/links',
+      'method' => 'GET',
+      'desc' => 'URL templates for generating URLs for HTML pages',
+    ),
+  ),
+);
+    return new JsonResponse($body);
+});
+
 // TODO: is there anything available from an elife library?
 $app->error(function (Throwable $e) {
     if ($e instanceof HttpExceptionInterface) {
---
+++ test/JavaScriptClientTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace test\eLife\Hypothesis;
+
+use PHPUnit_Framework_TestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+final class UsersTest extends PHPUnit_Framework_TestCase
+{
+    use SilexTestCase;
+
+    /**
+     * @test
+     */
+    public function it_provides_a_record_for_the_user()
+    {
+        $response = $this->getApp()->handle(Request::create(
+            '/',
+            'GET',
+            [],
+            [],
+            [],
+            [
+                'HTTP_HOST' => 'demo--hypothesis-dummy.elifesciences.org',
+                'HTTPS' => 'on',
+            ]
+        ));
+
+        $this->assertSame(
+            200,
+            $response->getStatusCode(),
+            var_export($response->getContent(), true)
+        );
+        $body = json_decode($response->getContent(), true);
+        $this->assertArrayHasKey('message', $body);
+        $this->assertEquals(
+            'Annotator Store API',
+            $body['message']
+        );
+        $this->assertArrayHasKey('links', $body);
+        $this->assertArrayHasKey('profile', $body['links']);
+        $this->assertArrayHasKey('read', $body['links']['profile']);
+        $this->assertArrayHasKey('url', $body['links']['profile']['read']);
+        $this->assertEquals('https://demo--hypothesis-dummy.elifesciences.org/profile', $body['links']['profile']['read']['url']);
+    }
+}
